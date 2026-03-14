8be5902615e693c297e6215ec536804159ac997d
dicolab4 <dico.colab4@gmail.com>
2026-03-14 15:14:15 -0300

    Update mapa

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,3 +1,4 @@
+// Código para o dashboard do administrador
 const map = L.map("map").setView([-22.52,-44.10],13)
 
 L.tileLayer(
@@ -15,6 +16,17 @@ if(cat==="iluminacao") return "yellow"
 if(cat==="mato") return "green"
 
 return "blue"
+
+}
+
+function formatarData(data){
+
+if(!data) return "-"
+
+const d = new Date(data)
+
+return d.toLocaleDateString("pt-BR")+" "+
+d.toLocaleTimeString("pt-BR")
 
 }
 
@@ -38,6 +50,15 @@ if(filtro && o.status!==filtro) return
 
 contador++
 
+// const icon = L.circleMarker(
+// [o.latitude,o.longitude],
+// {
+// color:corCategoria(o.categoria),
+// radius:8
+// })
+
+if(o.status !== "concluido"){
+
 const icon = L.circleMarker(
 [o.latitude,o.longitude],
 {
@@ -49,7 +70,10 @@ icon.bindPopup(`
 
 <b>${o.categoria}</b><br>
 ${o.descricao}<br>
+
 Status: ${o.status}<br>
+
+Criado em: ${formatarData(o.data_criacao)}<br>
 
 <img src="/uploads/${o.foto_url}" width="200">
 
@@ -63,6 +87,61 @@ Concluir
 
 cluster.addLayer(icon)
 
+}
+
+// icon.bindPopup(`
+
+// <b>${o.categoria}</b><br>
+// ${o.descricao}<br>
+// Status: ${o.status}<br>
+
+// <img src="/uploads/${o.foto_url}" width="200">
+
+// <br><br>
+
+// <button onclick="concluir(${o.id})">
+// Concluir
+// </button>
+
+// `)
+
+icon.bindPopup(`
+
+<b>${o.categoria}</b><br>
+${o.descricao}<br>
+
+Status: ${o.status}<br>
+
+Criado em: ${formatarData(o.data_criacao)}<br>
+
+Concluído em: ${formatarData(o.data_conclusao)}
+
+<br><br>
+
+<img src="/uploads/${o.foto_url}" width="200">
+
+<br><br>
+
+${o.status !== "concluido" ? 
+`<button onclick="concluir(${o.id})">Concluir</button>`
+: "Ocorrência resolvida"}
+
+`)
+
+cluster.addLayer(icon)
+
+// listaHTML += `
+// <div class="card">
+
+// <b>${o.categoria}</b>
+
+// <p>${o.descricao}</p>
+
+// <p>Status: ${o.status}</p>
+
+// </div>
+// `
+
 listaHTML += `
 <div class="card">
 
@@ -70,7 +149,11 @@ listaHTML += `
 
 <p>${o.descricao}</p>
 
-<p>Status: ${o.status}</p>
+<p class="status-${o.status}">Status: ${o.status}</p>
+
+<p>Criado em: ${formatarData(o.data_criacao)}</p>
+
+<p>Concluído em: ${formatarData(o.data_conclusao)}</p>
 
 </div>
 `
