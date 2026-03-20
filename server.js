f3d3ba294fd871cdefde3105d1b2bed73529de8a
dicolab4 <dico.colab4@gmail.com>
2026-03-20 11:26:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const usuariosRouter = require("./routes/usuarios")
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+//app.use(express.json())
+
+// Certifique-se de que estas linhas estão antes das rotas
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use("/uploads", express.static(path.join(__dirname,"uploads")))
 app.use(express.static(path.join(__dirname,"public")))
